a8dd7038edb25a63c217d0dc071046ff9b1b6b5b
kwenealete <kwenait@yahoo.ca>
2019-05-30 08:15:11 +0200

    added addIncome method, income array to store income and updated the getAccountSummary

--- expense-tracker.js
+++ expense-tracker.js
@@ -7,13 +7,27 @@ const account = {
             amount: amount
         });
     },
+    income: [], 
+    addIncome: function (description, amount) {
+        this.income.push({
+            description: description,
+            amount: amount
+        });
+    } ,
     getAccountSummary: function () {
         let totalExpenses = 0;
+        let totalIncome = 0;
+        let accountBalance = 0
         this.expenses.forEach(function (expense) {
             return totalExpenses = totalExpenses + expense.amount
-        })
-        return `${this.name} has $${totalExpenses} in expenses `
+        });
+        this.income.forEach(function(income) {
+            return totalIncome = totalIncome +income.amount
+        });
+        accountBalance = totalIncome - totalExpenses
+        return `${this.name} has an account balance of $${accountBalance}, with $${totalIncome} as income and $${totalExpenses} as expenses `
     }
 };
 account.addExpense('rent', 40000);
+account.addIncome('eent', 400000);
 console.log(account.getAccountSummary());
